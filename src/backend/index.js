52d3aa45c1116c70ab172f6fc0716331835ae11b
Yvan Acosta <acostacarrillo.yvan@gmail.com>
2025-06-03 07:19:12 -0700

    observemos

--- src/backend/index.js
+++ src/backend/index.js
@@ -4,4 +4,14 @@ import { connectToDatabase } from "../config/database.config.js";
 
 await connectToDatabase(); // asegura conexión antes de usar app
 
-export const handler = serverless(app);
+let handler;
+// Si se encuentra la variable de entorno SERVERLESS, se exporta el handler export const handler = serverless(app);
+if (process.env.SERVERLESS) {
+  handler = serverless(app);
+} else {
+  app.listen(app.get("port"), '0.0.0.0', () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+}
+
+export {handler};
